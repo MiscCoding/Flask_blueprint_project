bd429c113625f39a6a685b46c39a90ea486bfc70
neogeoss <neogeo-s@hanmail.net>
2018-09-02 23:38:07 +0900

    Item price crawling

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -1,45 +1,51 @@
 import uuid
-
+from bs4 import BeautifulSoup
+import requests
+import re
 from src.common.database import Database
 import src.models.items.constants as ItemConstants
+from src.models.stores.store import Store
 
-__author__ = "inchan"
-import requests
-from bs4 import BeautifulSoup
-import re
+__author__ = 'jslvtr'
+
 
 class Item(object):
-    def __init__(self, name, url, store, _id = None):
+    def __init__(self, name, url, price=None, _id=None):
         self.name = name
         self.url = url
-        self.store = store
-        tag_name = store.tag_name
-        query = store.query
-        self.price = self.load_item(tag_name, query)
+        store = Store.find_by_url(url)
+        self.tag_name = store.tag_name
+        self.query = store.query
+        self.price = None if price is None else price
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<Item {} with URL {}>".format(self.name, self.url)
 
-    def load_item(self, tag_name, query):
-        request = requests.get(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+    def load_price(self):
+        request = requests.get(self.url)
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
-        element = soup.find(tag_name, query)
+        element = soup.find(self.tag_name, self.query)
         string_price = element.text.strip()
 
         pattern = re.compile("(\d+.\d+)")
         match = pattern.search(string_price)
+        self.price = float(match.group())
 
-        self.price= match.group()
+        return self.price
 
     def save_to_mongo(self):
-        Database.insert(ItemConstants.COLLECTION, self.json())
+        Database.update(ItemConstants.COLLECTION, {'_id': self._id}, self.json())
 
     def json(self):
         return {
-            "name" : self.name,
-            "url" : self.url
+            "_id": self._id,
+            "name": self.name,
+            "url": self.url,
+            "price": self.price
         }
 
-
+    @classmethod
+    def get_by_id(cls, item_id):
+        return cls(**Database.find_one(ItemConstants.COLLECTION, {"_id": item_id}))
--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -40,3 +40,11 @@ class Store(object):
 
         return cls(**Database.find_one(StoreConstants.COLLECTION,{"url_prefix":{"$regex":"^{}".format(url_prefix)}}))
 
+    @classmethod
+    def find_by_url(cls, url):
+        for i in range(0, len(url)):
+            try:
+                store = cls.get_by_url_prefix(url[:i])
+                return store
+            except:
+                raise StoreErrors.StoreNotFoundException("The URL prefix used to find the store did not give us any result")
